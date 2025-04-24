d683f460cc75a2b8421ba6a59b1fed12ddee6f61
Zukertort <ricky.gobbi1994@gmail.com>
2025-04-23 23:10:57 -0300

    Registration Page

--- frontend-react/stock-prediction-portal/src/components/Header.jsx
+++ frontend-react/stock-prediction-portal/src/components/Header.jsx
@@ -1,16 +1,17 @@
 import React from 'react'
 import Button from './Button'
+import { Link } from 'react-router-dom'
 
 const Header = () => {
   return (
     <>
         <nav className='navbar conatiner pt-3 pb-3 px-5'>
-            <a className='navbar-brand text-light'>Stock Prediction Portal</a>
+            <Link className='navbar-brand' to='/'>Stock Prediction Portal</Link>
 
             <div>   
-                <Button text='Login' class='btn btn-outline-info' />
+                <Button text='Login' class='btn btn-outline-info' url='/login' />
                 &nbsp;
-                <Button text='Register' class='btn btn-info' />
+                <Button text='Register' class='btn btn-info' url='/register' />
             </div>
         </nav>
     </>
