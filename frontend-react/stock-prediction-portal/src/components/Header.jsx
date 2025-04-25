6a7ff15c317ed2181127ab168585d0e26d473c46
Zukertort <ricky.gobbi1994@gmail.com>
2025-04-25 06:13:34 -0300

    Logged in and out funtionality

--- frontend-react/stock-prediction-portal/src/components/Header.jsx
+++ frontend-react/stock-prediction-portal/src/components/Header.jsx
@@ -1,18 +1,38 @@
 import React from 'react'
 import Button from './Button'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
+import { AuthContext } from '../AuthProvider'
+import { useState, useContext, createContext } from 'react'
 
 const Header = () => {
+  const {isLoggedIn, setIsLoggedIn} = useContext(AuthContext)
+  const navigate = useNavigate();
+
+  const handleLogout = () => {
+    localStorage.removeItem('access_token')
+    localStorage.removeItem('refresh_token')
+    setIsLoggedIn(false)
+    console.log('Logout');
+    navigate('/login')
+  }
   return (
     <>
         <nav className='navbar conatiner pt-3 pb-3 px-5'>
             <Link className='navbar-brand' to='/'>Stock Prediction Portal</Link>
 
             <div>   
-                <Button text='Login' class='btn btn-outline-info' url='/login' />
-                &nbsp;
-                <Button text='Register' class='btn btn-info' url='/register' />
+              {isLoggedIn ? (
+                <button className='btn btn-danger' onClick={handleLogout}>Logout</button>
+              ) : (
+                  <>
+                  <Button text='Login' class='btn btn-outline-info' url='/login' />
+                  &nbsp;
+                  <Button text='Register' class='btn btn-info' url='/register' />   
+                  </> 
+                )
+              }
             </div>
+
         </nav>
     </>
   )
